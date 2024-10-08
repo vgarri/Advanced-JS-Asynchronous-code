ee728fe487d347d0a4902127a47b4d511d5d6e89
Victor Garritano <victor@getupgraded.com>
2024-10-08 18:15:35 +0200

    falta ej 9

--- index.js
+++ index.js
@@ -107,6 +107,8 @@ function getAndPrintGitHubUserProfile(username) {
 }
 
 
+
+
 // /* ```html
 // <section>
 //     <img src="url de imagen" alt="imagen de usuario">
@@ -118,3 +120,21 @@ function getAndPrintGitHubUserProfile(username) {
 
 
 //Ejercicio 9 usar Promise.all
+/*  9.- Dada una lista de usuarios de github guardada en una array,crea una funcion 
+fetchGithubUsers(userNames) que utilice 'https://api.github.com/users/${name}' para obtener el nombre de cada usuario.
+Objetivo: Usar Promise.all()
+Recordatorio: Una llamada a fetch() devuelve un objeto promesa.
+Pregunta. ¿cuántas promesas tendremos?
+Hasta que no se resuelvan todas las promesas desencadenadas por cada fetch(), no se cargarán los datos.
+
+Pasos:
+
+Mapear el array y hacer un fetch() para cada usuario. Esto nos de vuelve un array lleno de promesas.
+Con Promise.all() harás que se tenga que resolver todo el proceso de peticiones a GitHub a la vez.
+Cuando Promise.all() haya terminado: Consigue que se imprima por consola la url del 
+repositorio de cada usuario. Consigue que se imprima por consola el nombre de cada usuario.   */
+
+function fetchGitHubUsers(userNames){
+    let nombre =  getGitHubUserProfile('alenriquez96').then(data => data.name);
+    
+}
